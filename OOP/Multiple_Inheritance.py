1be2f4b27361772e58957adbfb7d8383c003ebb7
Shehraz Sarwar <sheeraz23445@gmail.com>
2025-03-11 07:19:38 +0500

    Add files via upload

---
+++ OOP/Multiple_Inheritance.py
@@ -0,0 +1,29 @@
+# Multiple inheritance
+
+class Human:
+    pass
+
+class Person(Human):
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def person_info(self):
+        print(f"Name: {self.name}\nAge: {self.age}")
+
+class Employee(Person,Human):
+    def __init__(self, name, age, id_no):
+        super().__init__(name,age)  #using person class Constructor super class
+        self.id_no = id_no
+
+    def emp_id(self):
+        print(f"Employee ID: {self.id_no}")
+
+    def emp_info(self):
+        print("Simple Employee")
+        super().person_info()
+        self.emp_id()
+
+
+employee = Employee("Jake",25,2313)
+employee.emp_info()
