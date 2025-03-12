0a804ed07780d86e3cf07df7f132d632b029dcee
Shehraz Sarwar <sheeraz23445@gmail.com>
2025-03-12 11:23:16 +0500

    Add files via upload

--- OOP/Property_Decorator.py
+++ OOP/Property_Decorator.py
@@ -1,1 +1,5 @@
 # More Advanced OOP Concept Property Decorator
+# @property = Decorator used to define a method as a property
+# It can be accessed like an attribute
+# Gives you setter, getter and deleter method
+
